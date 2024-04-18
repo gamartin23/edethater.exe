00d7f10b0ca7c16ae935b0f5b60ce16da1635a1d
gamartin23 <gamartingarcia37@gmail.com>
2024-04-18 20:55:04 -0300

    ahora tiene una api apropiada

--- script.js
+++ script.js
@@ -1,15 +1,55 @@
-let counter = localStorage.getItem('counter') || 0;
-let lastPressTime = localStorage.getItem('lastPressTime') || 0;
-document.getElementById('counter').innerText = counter;
-document.getElementById('button').addEventListener('click', function() {
-  const currentTime = new Date().getTime();
-  
-  if (currentTime - lastPressTime >= 30000) { 
-    counter++;
+let counter;
+let lastPressTime;
+let clicks = 0;
+let lastClickTime = 0;
+
+function getCounter() {
+  fetch('http://kovadev.pythonanywhere.com/counter')
+  .then(response => response.json())
+  .then(data => {
+    counter = data.counter;
     document.getElementById('counter').innerText = counter;
-    localStorage.setItem('counter', counter);
-    localStorage.setItem('lastPressTime', currentTime);
-  } else {
-    alert('Entendemos que tu bronca sea eterna, pero el hosting no lo es.');
-  }
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+}
+
+function incrementCounter() {
+    fetch('http://kovadev.pythonanywhere.com/increment',)
+    .then(response => response.json())
+    .then(data => {
+      counter = data.counter;
+      document.getElementById('counter').innerText = counter;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+function handleButtonClick() {
+  const currentTime = new Date().getTime();
+
+  if (clicks < 10) {
+      incrementCounter();
+      clicks++;
+      lastClickTime = currentTime;
+    } else {
+        if (currentTime - lastClickTime >= 120000) {
+            clicks = 0;
+            incrementCounter();
+            clicks++;
+            lastClickTime = currentTime;
+          } else {
+            alert('Bajale un cambio, volvé en dos minutitos');
+          }
+    }
+}a
+
+
+document.getElementById('button').addEventListener('click', handleButtonClick);
+
+document.addEventListener('DOMContentLoaded', function() {
+  getCounter();
+  lastPressTime = localStorage.getItem('lastPressTime') || 0;
 });
